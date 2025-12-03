b36b4a530200e9fe366644f72342cdcf6938aa1f
Shrihari Kamalan Kumaraguruparan <140620446+kkShrihari@users.noreply.github.com>
2025-12-03 22:37:11 +0100

    Create K_mers.py

---
+++ Python_exercises/K_mers.py
@@ -0,0 +1,28 @@
+class K_mers:
+    """
+    A class to compute unique and overlapping k-mers in a sequence.
+    """
+
+    def number_of_unique_kmers(self, s, k):
+        """
+        Extracts all unique non-overlapping k-mers from a sequence.
+        """
+        x = len(s)
+        y = x / k
+        
+        i = 0
+        A = 0
+        B = k
+        list0 = []
+        result1 = []
+        while i < y + k:
+            for z in s:
+                list1 = list(s[A:B])
+                A += k
+                B += k
+                if list1 != list0:
+                    if len(list1) == k:
+                       result = ["".join(map(str, list1))]
+                       result1.extend(result)
+                       i += 3
+        return result1
