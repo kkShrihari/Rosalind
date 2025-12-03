8043c371bc86138b6a941790c15d246391a5b297
Shrihari Kamalan Kumaraguruparan <140620446+kkShrihari@users.noreply.github.com>
2025-12-03 22:38:20 +0100

    Update K_mers.py

--- Python_exercises/K_mers.py
+++ Python_exercises/K_mers.py
@@ -26,3 +26,27 @@ class K_mers:
                        result1.extend(result)
                        i += 3
         return result1
+      
+    def list_kmers(self, s, k):
+        """
+        Extracts all overlapping k-mers from a sequence.
+        """
+        x = len(s)
+        y = x / k
+        
+        i = 0
+        A = 0
+        B = k
+        list0 = []
+        final_list = []
+        while i < k:
+            for z in s:
+                list1 = list(s[A:B])
+                A += 1
+                B += 1
+                if list1 != list0:
+                    if len(list1) == k:
+                        result = ["".join(map(str, list1))]
+                        final_list.extend(result)
+                        i += 1
+        return final_list
